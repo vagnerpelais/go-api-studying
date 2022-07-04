98c956e037d307575e8627d3548e3e5550034ac0
Vagner Bohm <vagner.cristiano@nappsolutions.com>
2022-07-04 11:56:57 -0300

    first commit

---
+++ api/go.mod
@@ -0,0 +1,12 @@
+module api
+
+go 1.18
+
+require (
+	github.com/badoux/checkmail v1.2.1 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+)
---
+++ api/src/router/rotas/rotas.go
@@ -0,0 +1,27 @@
+package rotas
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Rota representa todas as rotas da api
+type Rota struct {
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
+	RequerAutenticacao bool
+}
+
+// Configurar colocar todas as rotas no router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+	rotas = append(rotas, rotaLogin)
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
+}
